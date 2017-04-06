6969066a81dc3dfe720cfe354182d747454dc446
Eric Atempa <ea242@njit.edu>
2017-04-05 20:04:30 -0400

    End of HW14

---
+++ product_catalog/index.php
@@ -0,0 +1,40 @@
+<?php
+require('../model/database.php');
+require('../model/category.php');
+require('../model/category_db.php');
+require('../model/product.php');
+require('../model/product_db.php');
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL) {
+        $action = 'list_products';
+    }
+}  
+
+if ($action == 'list_products') {
+    $category_id = filter_input(INPUT_GET, 'category_id', 
+            FILTER_VALIDATE_INT);
+    if ($category_id == NULL || $category_id == FALSE) {
+        $category_id = 1;
+    }
+    $cat = new CategoryDB();
+    $dog = new ProductDB();
+    $current_category = $cat->getCategory($category_id);
+    $categories = $cat->getCategories();
+    $products = $dog->getProductsByCategory($category_id);
+
+    include('product_list.php');
+} else if ($action == 'view_product') {
+    $cat = new CategoryDB();
+    $dog = new ProductDB();
+    $categories = $cat->getCategories();
+
+    $product_id = filter_input(INPUT_GET, 'product_id', 
+            FILTER_VALIDATE_INT);   
+    $product = $dog->getProduct($product_id);
+
+    include('product_view.php');
+}
+?>
